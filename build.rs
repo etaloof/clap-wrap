bd898bff2c5fca69aa8a712d8e48f727ef69027f
Stefan Lukas <etaloof@gmail.com>
2023-01-07 15:09:36 +0100

    Remove cc build command

--- build.rs
+++ build.rs
@@ -1,24 +1,16 @@
+use std::path::PathBuf;
 use bindgen;
-use cc;
-use std::path::Path;
 
 fn main() {
-
-    cc::Build
-        ::new()        
-        .include(Path::new("C:/projects/RUST/clap-wrap/clap-main/include/clap"))
-        .includes(Path::new("C:/projects/RUST/clap-wrap/clap-main/src"))
-        .compile("clapc");
-
-    let bindings = 
+    let bindings =
         bindgen::Builder
             ::default()
             .header("wrapper.h")
             .generate()
             .expect("Failed to generate bindings");
 
-    bindings   
-        .write_to_file(Path::new("C:/projects/RUST/clap-wrap/src/bindings.rs"))
-        .expect("Failed to write bindings");
-
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
