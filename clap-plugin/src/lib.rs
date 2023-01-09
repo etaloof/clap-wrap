0896dad81277ec541ae44e6480f2b641e2bc4978
Stefan Lukas <etaloof@gmail.com>
2023-01-09 18:25:54 +0100

    Add clap plugin

---
+++ clap-plugin/src/lib.rs
@@ -0,0 +1,62 @@
+use clap_wrap::{
+    clap_host_t, clap_plugin_descriptor_t, clap_plugin_entry_t, clap_plugin_factory,
+    clap_plugin_factory_t, clap_plugin_t, clap_version_t, CLAP_PLUGIN_FACTORY_ID,
+};
+use libc::{c_char, c_void};
+use std::ptr::null_mut;
+
+// This constant corresponds to the `CLAP_VERSION_INIT` macro in `version.h`.
+const CLAP_VERSION_INIT: clap_version_t = clap_version_t {
+    major: 1,
+    minor: 1,
+    revision: 6,
+};
+
+#[no_mangle]
+pub static clap_entry: clap_plugin_entry_t = clap_plugin_entry_t {
+    clap_version: CLAP_VERSION_INIT,
+    init: Some(init),
+    deinit: Some(deinit),
+    get_factory: Some(get_factory),
+};
+
+unsafe extern "C" fn init(_: *const c_char) -> bool {
+    true
+}
+
+unsafe extern "C" fn deinit() {}
+
+unsafe extern "C" fn get_factory(factory_id: *const c_char) -> *const c_void {
+    let equal = unsafe { libc::strcmp(factory_id, CLAP_PLUGIN_FACTORY_ID.as_ptr()) };
+    if equal != 1 {
+        null_mut()
+    } else {
+        std::ptr::addr_of!(plugin_factory).cast()
+    }
+}
+
+#[no_mangle]
+pub static plugin_factory: clap_plugin_factory_t = clap_plugin_factory_t {
+    get_plugin_count: Some(get_plugin_count),
+    get_plugin_descriptor: Some(get_plugin_descriptor),
+    create_plugin: Some(create_plugin),
+};
+
+unsafe extern "C" fn get_plugin_count(factory: *const clap_plugin_factory) -> u32 {
+    todo!()
+}
+
+unsafe extern "C" fn get_plugin_descriptor(
+    factory: *const clap_plugin_factory,
+    index: u32,
+) -> *const clap_plugin_descriptor_t {
+    todo!()
+}
+
+unsafe extern "C" fn create_plugin(
+    factory: *const clap_plugin_factory,
+    host: *const clap_host_t,
+    plugin_id: *const c_char,
+) -> *const clap_plugin_t {
+    todo!()
+}
